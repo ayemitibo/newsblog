cd4e6770980ff43b5b0ee089a2cc44f1c90de514
Ayemitibo Abiodun <ayemitiboabiodun02@gmail.com>
2021-08-17 09:10:59 +0100

    choree : changed axios config

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -5,7 +5,10 @@ export const apiClient = axios.create({
   baseURL: "https://newsapi.org/v2/",
   headers: {
     Accept: "application/json",
-    'X-Api-Key' : '96ef503dc8664873a3299cab06b09f86'
+    'X-Api-Key' : '96ef503dc8664873a3299cab06b09f86',
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Methods' : "DELETE, POST, GET, OPTIONS",
+    "Access-Control-Allow-Headers" : "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
   }
 });
 apiClient.interceptors.request.use(config => {
