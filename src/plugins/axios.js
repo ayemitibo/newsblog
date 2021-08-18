b7cd93e64ddbb9b0391d6389ee461ad09870d6e8
Ayemitibo Abiodun <ayemitiboabiodun02@gmail.com>
2021-08-18 22:31:02 +0100

    chore : added new api key

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -11,7 +11,7 @@ apiClient.interceptors.request.use(config => {
   if(!config.url.startsWith("/")){
     return config
   } 
-  return  {...config,params :{ ...config.params,pageSize : 5,apiKey : "11cc01263f944b71af3311a37b02d63b"}};
+  return  {...config,params :{ ...config.params,pageSize : 5,apiKey : "96f29912297946b4b6ddb34d971579f5"}};
 });
 
 apiClient.interceptors.response.use(response => {
